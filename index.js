8755398443d2df824283a109c765ca75709ba922
Michael <michaelbracey24@gmail.com>
2022-07-19 16:07:32 +0100

    Added blocks

--- index.js
+++ index.js
@@ -26,8 +26,6 @@ const drawBall = () => {
     // } else {
     //     cntx.filter = 'blur(0px)';
     // }
-
-    cntx.beginPath()
     cntx.fillStyle = "red"
     cntx.fill()
     // end draw
@@ -38,17 +36,71 @@ const drawBall = () => {
     bally += dy;
 }
 
-const draw = () => {
+const game = () => {
     // remove previous drawings
     // cntx.clearRect(0, 0, canvas.width, canvas.height);
     cntx.fillStyle = 'rgba(255, 255, 255, .2)';
     cntx.fillRect(0, 0, canvas.width, canvas.height);
     // draw ball
+    drawBlocks()
     drawBall()
+    // drawpaddle()
     // add collisions
     collisions()
 }
 
+
+let blockHeight = 40
+let blockWidth = 80
+let blocks = [];
+let column = 7;
+let row = 3;
+
+//function that draws a block, and adds all appropriate keys
+function initialBlocks() {
+    for(let i = 0; i < row; i++){
+        for (let j = 0; j < column; j++){
+            blocks.push({
+                blockX: j * blockWidth,
+                blockY: i * blockHeight,
+                status: 1,
+            })
+        }
+    }
+
+    console.log(blocks)
+}
+
+
+function drawBlocks(){
+
+    for (let i = 0; i < blocks.length; i++){
+        console.log(blocks[i])
+        console.log(blocks[i].blockX)
+        if (blocks[i].status == 1){
+            cntx.beginPath()
+
+            // cntx.rect(x,y,width,height)
+            // cntx.rect(0,0,20,10)
+            // cntx.strokeStyle = 'blue';
+            cntx.rect(blocks[i].blockX + 10, blocks[i].blockY, blockWidth, blockHeight)
+            // fill color
+            cntx.fillStyle = "red"
+            cntx.strokeStyle = "#fff"
+            cntx.lineWidth = 2
+            if (i % 2 === 0) {
+                cntx.fillStyle = "blue"
+            }
+            cntx.fill()
+            cntx.stroke()
+            // cntx.blocks.addAtribute("")
+
+            // end draw
+            cntx.closePath();
+        }
+        
+    }
+}
 
 // collisions
 const collisions = () => {
@@ -63,4 +115,11 @@ const collisions = () => {
     // add a further check if at bottom wall end game?
 }
 
-setInterval(draw, 10);
+
+const startGame = () => {
+    initialBlocks()
+
+    setInterval(game, 10);
+}
+
+startGame()
